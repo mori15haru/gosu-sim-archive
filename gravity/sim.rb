7c73750bb2858023148e4e66da9f20215b6a8344
Miri Kim <miri.kim@onthebeach.co.uk>
2016-01-19 16:11:53 +0000

    P key turns on/off perspective

--- gravity/sim.rb
+++ gravity/sim.rb
@@ -2,46 +2,46 @@
 require 'gosu'
 require './particle'
 class SimWindow < Gosu::Window
-    @@w = 640
-    @@h = 480
-    @@t = 0.1
-    def initialize(n)
-        super @@w, @@h
-        self.caption = "Ruby :: Gosu :: Particle"
-        @particles = []
-        self.generate_particles(n)   
-    end
+  @@w = 640
+  @@h = 480
+  @@t = 0.1
+  def initialize(n)
+    super @@w, @@h
+    self.caption = "Ruby :: Gosu :: Particle"
+    @particles = []
+    self.generate_particles(n)   
+  end
 
-    def update
-        @particles.each do |p|
-            p.update
-        end
+  def update
+    @particles.each do |p|
+      p.update
     end
+  end
 
-    def draw
-        @particles.each do |p|
-            p.draw
-        end
+  def draw
+    @particles.each do |p|
+      p.draw
     end
-        
-    def generate_particles(n)
-        prng = Random.new
-        n.times{
-            x = prng.rand(@@w)
-            y = prng.rand(@@h)
-            vx = (-1)**x*prng.rand(30)
-            vy = (-1)**y*prng.rand(30)
-            ax = 0
-            ay = 9.8
-            @particles.push(Particle.new(x, y, vx, vy, ax, ay, @@w, @@h, @@t))
-        }
-    end
+  end
 
-    def button_down(id)
-        if id == Gosu::KbEscape
-            close
-        end
+  def generate_particles(n)
+    prng = Random.new
+    n.times{
+      x = prng.rand(@@w)
+      y = prng.rand(@@h)
+      vx = (-1)**x*prng.rand(30)
+      vy = (-1)**y*prng.rand(30)
+      ax = 0
+      ay = 9.8
+      @particles.push(Particle.new(x, y, vx, vy, ax, ay, @@w, @@h, @@t))
+    }
+  end
+
+  def button_down(id)
+    if id == Gosu::KbEscape
+      close
     end
+  end
 end
 
 window = SimWindow.new(ARGV[0].to_i)
