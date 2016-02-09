d58e20ec325e0a25f2fb87aa5f82f4b0aa16ccec
Miri Kim <miri.kim@onthebeach.co.uk>
2016-02-09 08:05:55 +0000

    Refactor

--- game-of-life/cell_pattern.rb
+++ game-of-life/cell_pattern.rb
@@ -1,51 +1,47 @@
 module CellPattern
   #Still lifes
   def self.block
-    c1 = Cell.new(CellState::ALIVE, [31,24])
-    c2 = Cell.new(CellState::ALIVE, [32,24])
-    c3 = Cell.new(CellState::ALIVE, [31,25])
-    c4 = Cell.new(CellState::ALIVE, [32,25])
-    [c1, c2, c3, c4]
+    seeds = [
+      [31,24], [32,24], [31,25], [32,25]
+    ]
+    
+    seeds.map{|seed| Cell.new(CellState::ALIVE, seed)}
   end
 
   #Oscillator
   def self.blinker
-    c1 = Cell.new(CellState::ALIVE, [31,24])
-    c2 = Cell.new(CellState::ALIVE, [32,24])
-    c3 = Cell.new(CellState::ALIVE, [33,24])
-    [c1, c2, c3]
+    seeds = [
+      [31,24], [32,24], [33,24]
+    ]
+    
+    seeds.map{|seed| Cell.new(CellState::ALIVE, seed)}
   end
 
   def self.toad
-    c1 = Cell.new(CellState::ALIVE, [31,25])
-    c2 = Cell.new(CellState::ALIVE, [32,25])
-    c3 = Cell.new(CellState::ALIVE, [32,24])
-    c4 = Cell.new(CellState::ALIVE, [33,25])
-    c5 = Cell.new(CellState::ALIVE, [33,24])
-    c6 = Cell.new(CellState::ALIVE, [34,24])
-    [c1, c2, c3, c4, c5, c6]
+    seeds = [
+      [31,25], [32,25], [32,24],
+      [33,25] [33,24], [34,24]
+    ]
+    
+    seeds.map{|seed| Cell.new(CellState::ALIVE, seed)}
   end
 
   def self.beacon
-    c1 = Cell.new(CellState::ALIVE, [31,23])
-    c2 = Cell.new(CellState::ALIVE, [32,23])
-    c3 = Cell.new(CellState::ALIVE, [31,22])
-    c4 = Cell.new(CellState::ALIVE, [32,22])
-    c5 = Cell.new(CellState::ALIVE, [33,21])
-    c6 = Cell.new(CellState::ALIVE, [34,21])
-    c7 = Cell.new(CellState::ALIVE, [33,20])
-    c8 = Cell.new(CellState::ALIVE, [34,20])
-    [c1, c2, c3, c4, c5, c6, c7, c8]
+    seeds = [
+      [31,23], [32,23], [31,22], [32,22],
+      [33,21], [34,21], [33,20], [34,20]
+    ]
+    
+    seeds.map{|seed| Cell.new(CellState::ALIVE, seed)}
   end
 
   #Spaceships
   def self.glinder
-    c1 = Cell.new(CellState::ALIVE, [5,5])
-    c2 = Cell.new(CellState::ALIVE, [3,6])
-    c3 = Cell.new(CellState::ALIVE, [5,6])
-    c4 = Cell.new(CellState::ALIVE, [4,7])
-    c5 = Cell.new(CellState::ALIVE, [5,7])
-    [c1, c2, c3, c4, c5]
+    seeds = [
+      [5,5], [3,6], [5,6], [4,7], [5,7]
+    ]
+    
+    seeds.map{|seed| Cell.new(CellState::ALIVE, seed)}
   end
 
   def self.gosper_glider_gun
